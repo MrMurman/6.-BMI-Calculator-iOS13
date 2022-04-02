8e7cc1509bd9dca7a8a60e64c289e5c90961f290
MrMurman <74971676+MrMurman@users.noreply.github.com>
2022-04-02 13:01:31 +0300

    Initial Commit

---
+++ 6. BMI-Calculator-iOS13/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  6. BMI-Calculator-iOS13
+//
+//  Created by Андрей Бородкин on 02.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
